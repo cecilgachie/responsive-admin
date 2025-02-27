b895185c27aad97eba42f568e085e8cf20a996a3
cecilgachie <159792219+cecilgachie@users.noreply.github.com>
2025-02-27 15:24:04 -0500

    Add files via upload

---
+++ responsive admin/order.js
@@ -0,0 +1,20 @@
+const Orders =[
+    {
+        productName: 'JavaScript tutorial',
+        productNumber: 85473,
+        productStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS full course',
+        productNumber: 85474,
+        productStatus: 'Available',
+        status: 'Declined'
+    },
+    {
+        productName: 'The complete HTML course',
+        productNumber: 34785,
+        productStatus: 'Refunded',
+        status: 'Active'
+    },
+]
